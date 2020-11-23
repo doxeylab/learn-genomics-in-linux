e798589d24239f93d8ffb89236f6b2b6c0b5c978
doxeylab <acdoxey@uwaterloo.ca>
2020-11-23 14:34:36 -0500

    Create runSalmon.bash

---
+++ task7/runSalmon.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for samp in SRR8451881 SRR8451882 SRR8451883 SRR8451884 SRR8451885 SRR8451886 SRR8451887 SRR8451888;
+do
+echo "Processing sample $samp"
+salmon quant -i gencode_v29_idx -l A \
+         -1 $samp_1.fastq.gz \
+         -2 $samp_2.fastq.gz \
+         -p 6  -o quants/${samp}_quant
+done
