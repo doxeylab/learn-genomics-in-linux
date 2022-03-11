9f6d3f7702b4e156f3c3ed3eacc0af3893879831
doxeylab <acdoxey@uwaterloo.ca>
2022-03-11 06:31:56 -0800

    Update runSalmon.bash

--- task7/runSalmon.bash
+++ task7/runSalmon.bash
@@ -3,8 +3,8 @@
 for samp in SRR8451881 SRR8451882 SRR8451883 SRR8451884 SRR8451885 SRR8451886 SRR8451887 SRR8451888;
 do
 echo "Processing sample $samp"
-salmon quant -i gencode_v29_idx -l A \
-         -1 data/${samp}_1.fastq.gz \
-         -2 data/${samp}_2.fastq.gz \
+salmon quant -i /fsys1/data/task4/gencode_v29_idx -l A \
+         -1 /fsys1/data/task4/${samp}_1.fastq.gz \
+         -2 /fsys1/data/task4/${samp}_2.fastq.gz \
          -p 6  -o quants/${samp}_quant
 done
